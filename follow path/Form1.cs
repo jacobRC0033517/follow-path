8bc10328ba1bcf0432ca377be12377cb43af43bc
jacob thornton <Jet2002Jake@gmail.com>
2018-09-18 09:49:27 +0100

    moves smoothly to the right

--- follow path/Form1.cs
+++ follow path/Form1.cs
@@ -16,5 +16,29 @@ namespace follow_path
         {
             InitializeComponent();
         }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <20; i++)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 4, pictureBox1.Location.Y);
+                await Task.Delay(100);
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y+4);
+                await Task.Delay(100);
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 4, pictureBox1.Location.X);
+                await Task.Delay(100);
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.Y + 4, pictureBox1.Location.Y);
+                await Task.Delay(100);
+            }
+        }
     }
 }
