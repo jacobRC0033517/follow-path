ea1394f5c17fc2c6825b0817ff47208d52d475a6
jacob thornton <Jet2002Jake@gmail.com>
2018-09-24 15:45:50 +0100

    added a textbox that changers the speed of image

--- follow path/Form1.cs
+++ follow path/Form1.cs
@@ -68,5 +68,15 @@ namespace follow_path
                 await Task.Delay(100);
             }
         }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+                await Task.Delay(Convert.ToInt32(textBox1.Text));
+            } 
+
+        }
     }
 }
