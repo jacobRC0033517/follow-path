1c2f15f5404d92fe579ed3f49f3a4c3243c55513
jacob thornton <Jet2002Jake@gmail.com>
2018-09-25 10:14:25 +0100

    added a message if letters are placed in the textbox instead of numbers

--- follow path/Form1.cs
+++ follow path/Form1.cs
@@ -71,6 +71,15 @@ namespace follow_path
 
         private async void button4_Click(object sender, EventArgs e)
         {
+            int d;
+            try
+            {
+                d = Convert.ToInt32(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("smart guy, use numbers");
+            }
             for (int i = 0; i < 5; i++)
             {
                 pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
